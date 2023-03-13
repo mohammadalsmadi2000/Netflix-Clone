65911544353f4902ea8a1d486888b7396b3e14f0
Mohammadalsmadi2000 <mohammadalsmadi.fcbayern@gmail.com>
2023-03-13 20:45:58 +0000

    Modify the homepage design

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home/Home';
 import NavbarNew from './components/Navbar/Navbar';
+import HeroSection from './components/TestComp/TestComp';
 
 function App() {
   return (
     <>
     <div className='div-App'>
     <NavbarNew />
+      <HeroSection/>
       <Home />
+
     </div>
 
 
